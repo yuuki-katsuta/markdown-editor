ac20d3d3ac3bbb37ee0f908f45c87fd80513c03a
yuuki-katsuta <reptpr0314@gmail.com>
2020-12-16 23:15:54 +0900

    styled-componentの導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import styled from 'styled-components'
 
-const Main = (<h1>Markdown Editor</h1>)
+//スタイリングされたコンポーネント作成
+//styled.(HTMLタグ名) で生成したい HTML タグを指定して、その後続く `` 内にCSSを記述する
+const Header = styled.h1`
+  color:red;
+`
+
+const Main = (<Header>Markdown Editor</Header>)
 //retunが省略されている
 //htmlを返すだけなので丸括弧で囲む
 
