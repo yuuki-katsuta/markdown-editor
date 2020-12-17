2a8dd315b13f6c5483d9ec3399d378e9b93fd88e
yuuki-katsuta <reptpr0314@gmail.com>
2020-12-17 23:10:37 +0900

    メイン画面のモックアップ作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import {Editor} from './pages/editor'
 
-//スタイリングされたコンポーネント作成
-//styled.(HTMLタグ名) で生成したい HTML タグを指定して、その後続く `` 内にCSSを記述する
-const Header = styled.h1`
-  color:red;
+//styled-components の createGlobalStyle を使って、ページ全体に適用できるスタイルを定義
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
 `
-
-const Main = (<Header>Markdown Editor</Header>)
-//retunが省略されている
-//htmlを返すだけなので丸括弧で囲む
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+)
 
 //app という ID を持つ HTML 内の要素に対して Main という変数の内容を紐付ける
 render(Main, document.getElementById('app'))
